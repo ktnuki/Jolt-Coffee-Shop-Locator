ce09d187de923dc7fb7458b11561b469b17197d9
Caitlyn Schoof <caitlynm1818@gmail.com>
2023-04-10 13:21:05 -0400

    updated schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -3,7 +3,6 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS user_address;
 DROP TABLE IF EXISTS shop_address;
 DROP TABLE IF EXISTS favorites;
-DROP TABLE IF EXISTS menu;
 DROP TABLE IF EXISTS address;
 DROP TABLE IF EXISTS hours;
 DROP TABLE IF EXISTS coffee_shops;
@@ -25,19 +24,11 @@ CREATE TABLE coffee_shops (
 	price_range int,
 	rating int,
 	highlights varchar(255),
+	menu_link varchar(255),
 	
 	CONSTRAINT pk_shop_id PRIMARY KEY (shop_id)
 );
 
-CREATE TABLE menu (
-	item_id serial,
-	shop_id int, 
-	item_name varchar(255),
-	item_image varchar(255), 
-	
-	CONSTRAINT pk_item_id PRIMARY KEY (item_id),
-	CONSTRAINT fk_shop_id FOREIGN KEY (shop_id) REFERENCES coffee_shops(shop_id)
-);
 
 CREATE TABLE hours (
 	hours_id serial,
@@ -87,5 +78,22 @@ CREATE TABLE favorites (
 	CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
 	CONSTRAINT fk_shop_id FOREIGN KEY (shop_id) REFERENCES coffee_shops(shop_id)
 );
+
+INSERT INTO coffee_shops (shop_name, main_image, website_link, price_range, rating, highlights, menu_link)
+VALUES ('Third Way Cafe', 'third way cafe_1611697786.webp', 'www.thirdwaycafe.org', 1, 4, 'coffee', 'www.thirdwaycafe.org/menu'),
+		('Fox in the Snow Cafe', 'foxinthesnow.jpeg', 'www.foxinthesnow.com', 3, 4, 'coffee, food, desserts, beans, alcohol', 'www.foxinthesnow.com/menus/#menu');
+		
+INSERT INTO address (address) 
+VALUES ('3058 W Broad St, Columbus, OH 43204'),
+		('1031 N 4th St, Columbus, OH 43201'),
+		('210 Thurman Ave, Columbus, OH 43206'),
+		('160 W Main St G, New Albany, OH 43054');
+
+INSERT INTO hours (shop_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+VALUES (1, '7a-3p', '7a-3p', '7a-3p', '7a-3p', '7a-3p', '8a-2p', '8a-2p'),
+		(2, '7a-3p', '7a-3p', '7a-3p', '7a-3p', '7a-3p', '8a-3p', '8a-3p');
+
+INSERT INTO shop_address (shop_id, address_id)
+VALUES (1, 1), (2, 2), (2, 3), (2, 4);
 
 COMMIT TRANSACTION;
