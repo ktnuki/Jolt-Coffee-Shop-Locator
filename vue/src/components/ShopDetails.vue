f1162a7865906e8ca924be92fab210e091a84aed
geoff.smith <gsmith5678@gmail.com>
2023-04-11 11:59:18 -0400

    push

--- vue/src/components/ShopDetails.vue
+++ vue/src/components/ShopDetails.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="shopdetails" >
-    Hello
-    {{ currentshop.address }}
-    {{ currentshop.shop }}
-    {{ currentshop.rating }}
+  <div id="shopdetails" >
+    <div id="sidebar-img">
+      <img src="" alt="coffee shop img">
+    </div>
+    <div> {{ currentshop.rating }} </div>
+    <div> {{ "price" }}  </div>
+    <div>{{ "menu" }}</div>
+    <div> 
+      <h5>{{ "hours" }}</h5>
+      <ul>
+        <li>Mon</li>
+        <li>Tue</li>
+        <li>Wed</li>
+        <li>Thur</li>
+        <li>Fri</li>
+        <li>Sat</li>
+        <li>Sun</li>
+      </ul>
+      </div>
+    <div>{{ currentshop.address }}</div>
+    
+    
+  
   </div>
 </template>
 
@@ -23,5 +41,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+#shopdetails{
+  height: 100vh;
+  display: grid;
+  grid-template-columns: auto;
+  align-items: center;
+  border-color: blueviolet;
+  border: solid;
+  border-width: 1px;
+}
+#shopdetails > div{
+  border-color: blueviolet;
+  border: solid;
+  border-width: 1px;
+}
+
 </style>
--- vue/src/views/Shops.vue
+++ vue/src/views/Shops.vue
@@ -1,40 +1,48 @@
 <template>
   <div>
-      <div>
-    <b-sidebar  v-model="isSideBarOpen" id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
-      <div class="px-3 py-2">
-        <shop-details />
-      </div>
-    </b-sidebar>
-  </div>
-      <shop-list @opensidebar="toggleSidebar()"/>
+    <div>
+      <b-sidebar
+        v-model="isSideBarOpen"
+        id="sidebar-variant"
+        bg-variant="dark"
+        text-variant="light"
+        aria-labelledby="sidebar-no-header-title"
+        no-header
+        shadow
+      >
+        <div class="px-3 py-2">
+          <h4 id="sidebar-no-header-title">{{ this.$store.state.activeShop.shop }}</h4>
+          <shop-details />
+        </div>
+      </b-sidebar>
+    </div>
+    <shop-list @opensidebar="toggleSidebar()" />
   </div>
 </template>
 
 <script>
-import ShopDetails from '../components/ShopDetails.vue'
-import ShopList from '../components/ShopList.vue'
+import ShopDetails from "../components/ShopDetails.vue";
+import ShopList from "../components/ShopList.vue";
 
 export default {
-    components: {
-        ShopList,
-        ShopDetails
+  components: {
+    ShopList,
+    ShopDetails,
+  },
+  data() {
+    return {
+      isSideBarOpen: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSideBarOpen = !this.isSideBarOpen;
     },
-    data(){
-      return{
-        isSideBarOpen: false
-      }
-    },
-    methods: {
-        toggleSidebar() {
-            this.isSideBarOpen = !this.isSideBarOpen;
-        }
-    }
-
-}
+  },
+  computed: {
+  },
+};
 </script>
 
 <style>
-
-
 </style>
