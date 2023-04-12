c1f1da4fb121b460360d3352b3b24947e5f702d4
Carter Turley <cwturley97@gmail.com>
2023-04-12 10:02:38 -0400

    added tests

---
+++ java/src/test/java/com/techelevator/dao/JdbcShopDaoTests.java
@@ -0,0 +1,39 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.CoffeeShop;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class JdbcShopDaoTests extends BaseDaoTests{
+
+    private static final CoffeeShop COFFEE_SHOP_1 = new CoffeeShop(1,"test_name", "test_image", "test_link", 1, 1, "test_highlights", "test_menu",
+            "9-5", "9-5", "9-5", "9-5", "9-5", "9-5", "9-5", "test_address");
+
+    private JdbcShopDao dao;
+
+
+    @Before
+    public void setup() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        dao = new JdbcShopDao(jdbcTemplate);
+    }
+
+    @Test
+    public void getShopList_finds_shop_information() {
+        List<CoffeeShop> coffeeShop = dao.getShopList();
+        Assert.assertEquals(1, coffeeShop.size());
+        Assert.assertEquals(COFFEE_SHOP_1.getShopId(), coffeeShop.get(0).getShopId());
+        Assert.assertEquals(COFFEE_SHOP_1.getShop(), coffeeShop.get(0).getShop());
+        Assert.assertEquals(COFFEE_SHOP_1.getAddress(), coffeeShop.get(0).getAddress());
+        Assert.assertEquals(COFFEE_SHOP_1.getMonday(), coffeeShop.get(0).getMonday());
+    }
+
+
+
+
+
+}
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -4,4 +4,17 @@ INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_US
 INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
 
+INSERT INTO coffee_shops (shop_name, main_image, website_link, price_range, rating, highlights, menu_link)
+VALUES ('test_name', 'test_image', 'test_link', 1, 1, 'test_highlights', 'test_menu');
+
+INSERT INTO address (address)
+VALUES ('test_address');
+
+INSERT INTO hours (shop_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+VALUES (1, '9-5', '9-5', '9-5', '9-5', '9-5', '9-5', '9-5');
+
+INSERT INTO shop_address (shop_id, address_id)
+VALUES (1, 1);
+
+
 COMMIT TRANSACTION;
