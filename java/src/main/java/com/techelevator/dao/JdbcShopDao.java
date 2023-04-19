9f228b9cc43d55732a8b3290a7d7252878cf4461
Caitlyn Schoof <caitlynm1818@gmail.com>
2023-04-19 13:39:12 -0400

    java fix

--- java/src/main/java/com/techelevator/dao/JdbcShopDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcShopDao.java
@@ -26,7 +26,7 @@ public class JdbcShopDao implements ShopDao {
                 "JOIN shop_address ON shop_address.shop_id = coffee_shops.shop_id\n" +
                 "JOIN address ON shop_address.address_id = address.address_id\n" +
                 "JOIN hours ON hours.shop_id = coffee_shops.shop_id\n" +
-                           //TODO: add true only
+                "WHERE isApproved = true\n" +           //TODO: added this to not show anything false
                 "ORDER BY shop_name";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         while (results.next()) {
